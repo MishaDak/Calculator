072471754d166c05da7d3ee8b6fa8821f99cad79
MishaDak <dack.misha@gmail.com>
2019-04-29 23:11:22 +0700

    Add output in main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "input.h"
 #include "func.h"
 
-void input(char** str, int* mistake);
+void input(char **str,int* mistake);
+double Calculation(char **str, int Start, int End);
 
 int main()
 {
@@ -19,6 +21,9 @@ int main()
 			printf("%c", str[i]);
 		}
 	}
+
+	double result = Calculation(&str, 0, z - 1);
+	printf("\t\t%s = %lf\n", str, result);
 
 	return 0;
 }
